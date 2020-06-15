6b1cd9e06d323475ff269c49ac747accc471ea9f
Matheus30cs <62408199+Matheus30cs@users.noreply.github.com>
2020-06-15 07:36:42 -0300

    Update Modelo Físico - Consulta no Banco.sql

--- Modelo Físico - Consulta no Banco.sql
+++ Modelo Físico - Consulta no Banco.sql
@@ -24,3 +24,34 @@ Quais Alimentos um Cozinheiro está preparando?
 
 select A.nome, B.alimento from Funcionario A, Cozinheiro B
 where A.id_funcionario = B.id_funcionario;
+
+O entregador X trabalha em qual empresa?
+
+select A.nome, B.nomeEmpresa from Funcionario A, Empresa B, Entregador C
+where A.id_empresa = B.id_empresa and A.id_funcionario = C.id_funcionario;
+
+O cozinheiro X trabalha em qual empresa?
+
+select A.nome, B.nomeEmpresa from Funcionario A, Empresa B, Cozinheiro C
+where A.id_empresa = B.id_empresa and A.id_funcionario = C.id_funcionario;
+
+A entrega X foi feita por qual entregador?
+select A.nome, B.pedidoEntrega from Funcionario A, Entregador B;
+
+Qual entregador tem a maior nota?
+select MAX(nota) from Entregador;
+
+A comida do pedido X foi entregue por qual entregador?
+
+select C.pedidoEntrega, B.Nome from Pedido A, Funcionario B, Entregador C
+where B.id_funcionario = C.id_funcionario and A.id_pedido = C.id_pedido;
+
+A comida X do pedido Y foi feita por qual cozinheiro?
+select C.alimento, B.Nome from Pedido A, Funcionario B, Cozinheiro C
+
+where B.id_funcionario = C.id_funcionario and A.id_cozinheiro = C.id_cozinheiro;
+
+A comida X é mais ligada a qual empresa?
+Qual é o bairro com o maior numero de clientes?
+
+Qual é a comida mais pedida em certo bairro?
